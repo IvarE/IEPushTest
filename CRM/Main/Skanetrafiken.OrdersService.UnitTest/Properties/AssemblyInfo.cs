c89bff57ae92f45acedffa6620d683c09162684e
Pedro Gonçalo <pedro.goncalo@endeavor.se>
2020-05-12 16:08:24 +0100

    #First Push Orders Service Structure Project

---
+++ CRM/Main/Skanetrafiken.OrdersService.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Skanetrafiken.OrdersService.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Endeavor")]
+[assembly: AssemblyProduct("Skanetrafiken.OrdersService.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0bd3a1ec-2734-47eb-89a5-151ab7d27c8e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
