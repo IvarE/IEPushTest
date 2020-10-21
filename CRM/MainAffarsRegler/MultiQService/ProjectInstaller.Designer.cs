925f22ff0ea794e4ea0b699dd5e2c09bde5083a8
Pedro Gonçalo <pedro.goncalo@findmore.eu>
2020-10-21 15:24:55 +0100

    #Add Project Installer

--- CRM/MainAffarsRegler/MultiQService/ProjectInstaller.Designer.cs
+++ CRM/MainAffarsRegler/MultiQService/ProjectInstaller.Designer.cs
@@ -28,9 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "Upload File to Sekund from MultiQ";
+            this.serviceInstaller1.DisplayName = "MultiQService";
+            this.serviceInstaller1.ServiceName = "MultiQService";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
         }
 
         #endregion
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
     }
 }
