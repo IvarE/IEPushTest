6fcbf4c08bed8baadf4cac15eff3c426008262f7
Pedro Gonçalo <pedro.goncalo@findmore.eu>
2020-10-28 21:11:21 +0000

    #Add new service MultiQ Service

---
+++ CRM/MainAffarsRegler/SkaneMultiQService/Model/MultiQFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Endeavor.Crm.MultiQService.Model
+{
+    public class MultiQFile
+    {
+        public string fileName { get; set; }
+        public string filePath { get; set; }
+    }
+}
---
+++ CRM/MainAffarsRegler/SkaneMultiQService/ServiceProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace Endeavor.Crm.MultiQService
+{
+    class ServiceProvider : IServiceProvider
+    {
+        object IServiceProvider.GetService(Type serviceType)
+        {
+            if (serviceType == typeof(ITracingService))
+                return new TracingService();
+
+            throw new NotImplementedException();
+        }
+    }
+}
