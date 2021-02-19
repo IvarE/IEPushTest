eac33dd9685a0a2cf3d1b0969c283e248f4d66ca
José Almeida <jose.almeida@endeavor.se>
2021-02-19 15:50:38 +0000

    development of SlotsGenerator

---
+++ CRM/MainAffarsRegler/Skanetrafiken.Crm.Workflow/GenerateSlots.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using Endeavor.Crm;
+using System.Runtime.Serialization;
+
+namespace Skanetrafiken.Crm.Entities
+{
+    public class GenerateSlots : CodeActivity
+    {
+        [Input("ProductID")]
+        public InArgument<string> ProductId { get; set; }
+        [Input("QuantityPerDay")]
+        public InArgument<int> QuantityPerDay { get; set; }
+
+        [Input("StartDate")]
+        public InArgument<DateTime> StartDate { get; set; }
+
+        [Input("EndDate")]
+        public InArgument<DateTime> EndDate { get; set; }
+
+        [Output("OK")]
+        public OutArgument<string> OK { get; set; }
+
+        [Output("Message")]
+        public OutArgument<string> Message { get; set; }
+
+        private Plugin.LocalPluginContext GetLocalContext(CodeActivityContext activityContext)
+        {
+            IWorkflowContext workflowContext = activityContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = activityContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService organizationService = serviceFactory.CreateOrganizationService(workflowContext.InitiatingUserId);
+            ITracingService tracingService = activityContext.GetExtension<ITracingService>();
+
+
+            return new Plugin.LocalPluginContext(null, organizationService, null, tracingService);
+        }
+
+        protected override void Execute(CodeActivityContext activityContext)
+        {
+            //TRY EXECUTE
+            try
+            {
+                //GENERATE CONTEXT
+                Plugin.LocalPluginContext localContext = GetLocalContext(activityContext);
+
+                localContext.TracingService.Trace($"GenerateSlots started.");
+
+                //GET VALUE(S)
+                string product = ProductId.Get(activityContext);
+                int quantityPerDay = QuantityPerDay.Get(activityContext);
+                DateTime startDate = StartDate.Get(activityContext);
+                DateTime endDate = EndDate.Get(activityContext);
+
+                if(string.IsNullOrEmpty(product))
+                {
+                    OK.Set(activityContext, false);
+                    Message.Set(activityContext, "No Product selected.");
+
+                    return;
+                }
+
+                Guid productId = Guid.Parse(product);
+
+                SlotsEntity.GenerateSlotsResponse response = ExecuteCodeActivity(localContext, productId, quantityPerDay,startDate,endDate);
+                localContext.Trace($"GenerateSlots finished. Success: {response.OK} | Message: {response.Message}");
+
+                OK.Set(activityContext, response.OK);
+                Message.Set(activityContext, response.Message);
+
+            }
+            catch (Exception ex)
+            {
+                OK.Set(activityContext, false);
+                Message.Set(activityContext, $"Kunde inte skapa värdkod. Vänligen kontakta kundtjänst. StripError (GenerateSlots): { ex.Message}");
+            }
+        }
+        public static SlotsEntity.GenerateSlotsResponse ExecuteCodeActivity(Plugin.LocalPluginContext localContext, Guid productId, int quantityPerDay, DateTime startDate, DateTime endDate)
+        {
+            SlotsEntity.GenerateSlotsResponse response = SlotsEntity.GenerateSlots(localContext, productId, quantityPerDay, startDate, endDate);
+
+            return response;
+        }
+    }
+
+    
+}
---
+++ CRM/MainAffarsRegler/Skanetrafiken.Crm/Entities/SlotsEntity.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generated = Skanetrafiken.Crm.Schema.Generated;
+using Endeavor.Crm;
+using System.Runtime.Serialization;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+
+namespace Skanetrafiken.Crm.Entities
+{
+    public class SlotsEntity : Generated.ed_Slots
+    {
+        [DataContract]
+        public class GenerateSlotsResponse
+        {
+            [DataMember]
+            public bool OK { get; set; }
+
+            [DataMember]
+            public string Message { get; set; }
+        }
+
+
+        public static GenerateSlotsResponse GenerateSlots (Plugin.LocalPluginContext localContext,Guid productId, int quantityPerDay, DateTime startDate, DateTime endDate)
+        {
+
+            GenerateSlotsResponse response = new GenerateSlotsResponse();
+
+            if (endDate < startDate)
+            {
+                response.OK = false;
+                response.Message = "Start date is higher than the End date.";
+            }
+
+            FeatureTogglingEntity feature = FeatureTogglingEntity.GetFeatureToggling(localContext, FeatureTogglingEntity.Fields.ed_bookingsystem);
+            
+
+            ColumnSet productColumns = new ColumnSet(false);
+            productColumns.AddColumn(ProductEntity.Fields.Price);
+
+            Money priceProduct = new Money(0);
+
+            
+
+            ProductEntity product = XrmRetrieveHelper.Retrieve<ProductEntity>(localContext, productId, productColumns);
+
+            if(product != null)
+            {
+                //if (product.Contains(ProductEntity.Fields.Price) && product.Price != null)
+                    //priceProduct = product.;
+            }
+            else
+            {
+                response.OK = false;
+                response.Message = "Product not found.";
+                return response;
+            }
+
+            do
+            {
+                
+            }
+            while (DateTime.Compare(startDate,endDate) <= 0);
+            
+            
+
+            return response;
+        }
+
+    }
+
+
+
+}
