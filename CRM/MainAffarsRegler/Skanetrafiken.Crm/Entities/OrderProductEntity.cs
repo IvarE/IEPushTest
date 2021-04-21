283fb46f0c3f986774d40f53b4aef1d4f3dccf86
José Almeida <jose.almeida@endeavor.se>
2021-04-21 13:49:51 +0200

    started OrderProduct Development

--- CRM/MainAffarsRegler/Skanetrafiken.Crm/Entities/OrderProductEntity.cs
+++ CRM/MainAffarsRegler/Skanetrafiken.Crm/Entities/OrderProductEntity.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Endeavor.Crm;
+using Endeavor.Crm.Extensions;
+using Microsoft.Crm.Sdk;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Collections.Generic;
 using Generated = Skanetrafiken.Crm.Schema.Generated;
 
 namespace Skanetrafiken.Crm.Entities
 {
     public class OrderProductEntity : Generated.SalesOrderDetail
     {
+        public static void HandleOrderProductEntityCreate (Plugin.LocalPluginContext localContext, OrderProductEntity orderProduct)
+        {
+
+        }
     }
 }
