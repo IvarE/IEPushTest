98ab80af1264487723ab97f99e41523dea6fef41
Pedro Gonçalo <pedro.goncalo@findmore.eu>
2021-06-01 17:15:56 +0100

    #8066: Pugin for SSIS

---
+++ CRM/MainAffarsRegler/Skanetrafiken.Crm/Entities/TicketInfoEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Generated = Skanetrafiken.Crm.Schema.Generated;
+
+namespace Skanetrafiken.Crm.Entities
+{
+    public class TicketInfoEntity : Generated.ed_ticketinfo
+    {
+    }
+}
