c276532ac57e2634b169f67e53c26470de0f23d9
Jose.almeida <Jose.almeida@endeavor.se>
2021-06-21 17:12:22 +0100

    fixed issue with query

--- CRM/MainAffarsRegler/Skanetrafiken.Crm/Entities/SlotsEntity.cs
+++ CRM/MainAffarsRegler/Skanetrafiken.Crm/Entities/SlotsEntity.cs
@@ -129,7 +129,7 @@ namespace Skanetrafiken.Crm.Entities
 
             FilterExpression filterSlotDayProduct = new FilterExpression();
             filterSlotDayProduct.FilterOperator = LogicalOperator.And;
-            filterSlotDayProduct.AddCondition(SlotsEntity.Fields.ed_BookingDay, ConditionOperator.Equal, target.ed_BookingDay);
+            filterSlotDayProduct.AddCondition(SlotsEntity.Fields.ed_BookingDay, ConditionOperator.Equal, target.ed_BookingDay.Value);
             filterSlotDayProduct.AddCondition(SlotsEntity.Fields.ed_ProductID, ConditionOperator.Equal, target.ed_ProductID);
 
             querySlotDayProduct.Criteria.AddFilter(filterSlotDayProduct);
