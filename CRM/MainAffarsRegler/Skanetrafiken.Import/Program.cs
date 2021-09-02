5ff81786fd6e90f7a2e5065a67796558d266f9a0
Pedro Gonçalo <pedro.goncalo@findmore.eu>
2021-09-02 14:21:25 +0100

    #Clean Singapore Duplicates

--- CRM/MainAffarsRegler/Skanetrafiken.Import/Program.cs
+++ CRM/MainAffarsRegler/Skanetrafiken.Import/Program.cs
@@ -21,6 +21,7 @@ using System.Linq;
 using System.ServiceModel.Description;
 using System.Text;
 using System.Threading;
+using System.Xml;
 
 namespace Skanetrafiken.Import
 {
@@ -2815,11 +2816,50 @@ namespace Skanetrafiken.Import
             Console.WriteLine("Done.");
         }
 
+        public static string CreateXml(string xml, string cookie, int page, int count)
+        {
+            StringReader stringReader = new StringReader(xml);
+            XmlTextReader reader = new XmlTextReader(stringReader);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(reader);
+            XmlAttributeCollection attrs = doc.DocumentElement.Attributes;
+            if (cookie != null)
+            {
+                XmlAttribute pagingAttr = doc.CreateAttribute("paging-cookie");
+                pagingAttr.Value = cookie;
+                attrs.Append(pagingAttr);
+            }
+            XmlAttribute pageAttr = doc.CreateAttribute("page");
+            pageAttr.Value = System.Convert.ToString(page);
+            attrs.Append(pageAttr);
+            XmlAttribute countAttr = doc.CreateAttribute("count");
+            countAttr.Value = System.Convert.ToString(count);
+            attrs.Append(countAttr);
+            StringBuilder sb = new StringBuilder(1024);
+            StringWriter stringWriter = new StringWriter(sb);
+            XmlTextWriter writer = new XmlTextWriter(stringWriter);
+            doc.WriteTo(writer);
+            writer.Close();
+            return sb.ToString();
+        }
+
+
         public static void CleanSingaporeDuplicates(Plugin.LocalPluginContext localContext, CrmContext crmContext)
         {
             try
             {
-                string fetchXMl = @"<fetch distinct='false' mapping='logical' aggregate='true' >
+                // Define the fetch attributes.
+                // Set the number of records per page to retrieve.
+                int fetchCount = 5000;
+                // Initialize the page number.
+                int pageNumber = 1;
+                // Initialize the number of records.
+                int recordCount = 0;
+                // Specify the current paging cookie. For retrieving the first page, 
+                // pagingCookie should be null.
+                string pagingCookie = null;
+
+                string fetchXml = @"<fetch distinct='false' mapping='logical' output-format='xml-platform' aggregate='true' >
                                   <entity name='st_singaporeticket' >
                                     <attribute name='st_singaporeticketid' alias='singapore_count' aggregate='countcolumn' />
                                     <attribute name='st_contactid' alias='st_ContactID' groupby='true' />
@@ -2827,7 +2867,38 @@ namespace Skanetrafiken.Import
                                   </entity>
                                 </fetch>";
 
-                EntityCollection ecSingapore = localContext.OrganizationService.RetrieveMultiple(new FetchExpression(fetchXMl));
+                EntityCollection fullData = new EntityCollection();
+
+                while (true)
+                {
+                    // Build fetchXml string with the placeholders.
+                    string xml = CreateXml(fetchXml, pagingCookie, pageNumber, fetchCount);
+
+                    // Excute the fetch query and get the xml result.
+                    RetrieveMultipleRequest fetchRequest1 = new RetrieveMultipleRequest
+                    {
+                        Query = new FetchExpression(xml)
+                    };
+
+                    EntityCollection returnCollection = ((RetrieveMultipleResponse)localContext.OrganizationService.Execute(fetchRequest1)).EntityCollection;
+
+                    fullData.Entities.AddRange(returnCollection.Entities);
+
+                    // Check for morerecords, if it returns 1.
+                    if (returnCollection.MoreRecords)
+                    {
+                        // Increment the page number to retrieve the next page.
+                        pageNumber++;
+
+                        // Set the paging cookie to the paging cookie returned from current results.                            
+                        pagingCookie = returnCollection.PagingCookie;
+                    }
+                    else
+                    {
+                        // If no more records in the result nodes, exit the loop.
+                        break;
+                    }
+                }
 
 
             }
