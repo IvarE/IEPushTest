85b73de44ae2a878360fddf9a1f50de87becf814
Markus Erlandsson <markus.erlandsson@absfront.com>
2023-05-04 13:57:44 +0000

    Deleted Release 1.13.0

--- DK/Development/Release 1.13.0/SQL/SkanetrafikenDatabase/DKCRM_MSCRM/dbo/Tables/PhoneCallBase.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[PhoneCallBase] (
-    [ActivityId]          UNIQUEIDENTIFIER NOT NULL,
-    [cgi_CreateCase]      BIT              NULL,
-    [cgi_CallguideInfoid] UNIQUEIDENTIFIER NULL,
-    CONSTRAINT [PK_PhoneCallBase] PRIMARY KEY CLUSTERED ([ActivityId] ASC) WITH (FILLFACTOR = 80),
-    CONSTRAINT [cgi_cgi_callguideinfo_phonecall_CallguideInfoid] FOREIGN KEY ([cgi_CallguideInfoid]) REFERENCES [dbo].[cgi_callguideinfoBase] ([cgi_callguideinfoId]) NOT FOR REPLICATION,
-    CONSTRAINT [FK_PhoneCallBase_ActivityPointerBase] FOREIGN KEY ([ActivityId]) REFERENCES [dbo].[ActivityPointerBase] ([ActivityId]) NOT FOR REPLICATION
-);
-
